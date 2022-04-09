962970b165bbbad4883d0cc0d4c369b7e034aba6
Anoop <anoopsukumarannairk@gmail.com>
2022-04-09 05:15:16 +0100

    to do list counter added at the footer.

--- script.js
+++ script.js
@@ -6,6 +6,8 @@
     let task;
     let tasklist;
     let todoListArray = [];
+    let counter = 0;
+    let count = document.getElementById("todoCounter");
 
     addButton.addEventListener('click',(e) => {
         e.preventDefault();
@@ -19,8 +21,11 @@
         setTodoList();
         getTodoList();
         }
-        inputVal.value = "";
-       inputVal.setAttribute("placeholder","please type your to do list");
+        
+    inputVal.value = "";
+    inputVal.setAttribute("placeholder","please type your to do list");
+    counter++;
+    count.innerText = counter;
         
     });
    
@@ -35,6 +40,7 @@
      if(localStorage.getItem("Todolist")) {
      todoListArray = JSON.parse(localStorage.getItem("Todolist"));
       buildLi();
+      
      }
      }
     
@@ -52,27 +58,27 @@
         tasklist.setAttribute("contenteditable","false");
         document.getElementById("content").append(tasklist);
         tasklist.innerText = item;
-
-       
-        
         
         const del = document.createElement('button');
         del.classList.add("delete");
         del.innerText = 'Delete';
         tasklist.appendChild(del);
+
+       
+
         del.addEventListener('click',() => {
 
             todoListArray =JSON.parse(localStorage.getItem("Todolist"));
             todoListArray.splice(index,1);
             localStorage.setItem("Todolist",JSON.stringify(todoListArray));
             getTodoList();
-
-                 
-        
-            
+            counter--;
+            count.innerText = counter;
         });
        
+       
     });
+    
     
     }
     getTodoList();
